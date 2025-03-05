4d992d792139b9a32a11307d70657f7e80dbdf7e
allanknecht <allanknecht@gmail.com>
2025-03-04 21:51:44 -0300

    added a lot of things

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "home/index"
+  resources :articles
+
+  # Define a rota raiz da aplicação
+  root "home#index"
+
+  # Health check do Rails
+  get "up" => "rails/health#show", as: :rails_health_check
+end
