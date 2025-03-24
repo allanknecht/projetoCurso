9db920c6517c810a69a3822a75fb65a94cd700a9
allanknecht <allanknecht@gmail.com>
2025-03-23 21:13:38 -0300

    addd edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to articles_path, notice: "User #{@user.username} was successfully updated." }
+        # format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def user_params
